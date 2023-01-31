a50f0b262acaa944f379699754886c3f34245d99
CDConlyred <1746904635@qq.com>
2023-01-31 15:58:05 +0800

    Redis生成全局唯一id

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,33 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    private static final int COUNT_BITS = 32;  // 序列号的位数
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RedisIdWorker(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public long nextId(String keyPrefix) {
+        // 生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = nowSecond - BEGIN_TIMESTAMP;
+        // 生成序列号
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        Long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        // 拼接
+        return timeStamp << COUNT_BITS | count;
+    }
+}
